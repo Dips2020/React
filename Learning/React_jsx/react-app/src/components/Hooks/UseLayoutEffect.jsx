aca659ce63a450c930dd2a7589e8ebf2a6cb8646
Dips2020 <dipsbhattarai2020@gmail.com>
2023-12-07 21:57:00 +0545

    learned about useLayoutEffect hook

---
+++ Learning/React_jsx/react-app/src/components/Hooks/UseLayoutEffect.jsx
@@ -0,0 +1,36 @@
+// 1⬇️ ---------------------------------------------------
+// // Syntax of useEffect and useLayoutEffect hooks are same
+// import { useEffect, useLayoutEffect } from "react";
+// const UseLayoutEffect = () => {
+//   // ! useLayoutEffect is always executed first (it is a initial stage before printing in DOM)
+//   useEffect(() => {
+//     console.log("This is useEffect hook.");
+//   }, []);
+//   useLayoutEffect(() => {
+//     console.log("This is useLayoutEffect hook.");
+//   }, []);
+//   return <div>UseLayoutEffect</div>;
+// };
+
+// export default UseLayoutEffect;
+
+// 2⬇️ ---------------------------------------------------
+import { useEffect, useLayoutEffect, useState } from "react";
+const UseLayoutEffect = () => {
+  const [count, setCount] = useState(0);
+  useEffect(() => {
+    console.log("This is useEffect hook.");
+  }, [count]);
+  useLayoutEffect(() => {
+    console.log("This is useLayoutEffect hook.");
+  }, [count]);
+  return (
+    <div>
+      UseLayoutEffect
+      <br />
+      <button onClick={() => setCount(count + 1)}>Increase</button>
+    </div>
+  );
+};
+
+export default UseLayoutEffect;
